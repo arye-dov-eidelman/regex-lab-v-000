d72d407d917b62e9f7d513a8607dda1b6694e14e
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-03-07 16:59:02 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -5,11 +5,11 @@ end
 
 def words_starting_with_un_and_ending_with_ing(text)
   text.scan(/un\w*ing/)
-  binding.pry
 end
 
 def words_five_letters_long(text)
   text.scan(/^\w(\w{5})^\w/)
+  binding.pry
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
